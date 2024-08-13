36dfb373b55584610c268a8fcac50ef8921e1a5f
Patrick Hobusch <patrick.hobusch@deftdevs.com>
2024-08-13 18:36:30 +0200

    Update and improve flake, simplify supported systems, add Go dev shell

--- flake.nix
+++ flake.nix
@@ -1,55 +1,47 @@
 {
-  description = "A simple Go package";
+  description = "Caddy with Hetzner DNS plugin and a Go dev shell";
 
-  # Nixpkgs / NixOS version to use. 
-  # As of 2023-10-04 we need unstable for Go # 1.20
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  # The NixOS version must contain the required Go version as described in:
+  # https://github.com/caddyserver/caddy?tab=readme-ov-file#build-from-source
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
-  outputs = { self, nixpkgs }:
+    systems.url = github:nix-systems/default;
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs = {
+        systems.follows = "systems";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
     let
-
       # to work with older version of flakes
       lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
 
       # Generate a user-friendly version number.
       version = builtins.substring 0 8 lastModifiedDate;
-
-      # System types to support.
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-
-      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-      # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-
     in
-    {
 
-      # # A Nixpkgs overlay.
-      # overlay = final: prev: {
-      #   go = final.go_1_20;
-      #   buildGoModule = final.buildGo117Module;
-      # };
-
-      # Provide some binary packages for selected system types.
-      packages = forAllSystems (system:
+    flake-utils.lib.eachDefaultSystem
+      (system:
         let
-          pkgs = nixpkgsFor.${system};
+          pkgs = nixpkgs.legacyPackages.${system};
         in
         {
-
-          caddy = pkgs.buildGoModule {
+          packages.caddy = pkgs.buildGoModule {
             pname = "caddy";
             inherit version;
             src = ./caddy-src;
             runVend = true;
-            vendorHash = "sha256-o5s3i+HArqXcmnhmpnnm1qEKmU/UeYii13Qoj5nP39A=";
-            # vendorHash = pkgs.lib.fakeSha256;
-
+            vendorHash = "sha256-o5s3i+HArqXcmnhmpnnm1qEKmU/UeYii13Qoj5nP39A="; # pkgs.lib.fakeSha256
           };
-        });
 
-      defaultPackage = forAllSystems (system: self.packages.${system}.caddy);
-    };
+          defaultPackage = self.packages.${system}.caddy;
+
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        }
+      );
 }
